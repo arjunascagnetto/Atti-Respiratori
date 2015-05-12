2fe74d4a55f5dcb4d975792023f0c25e980ebb6a
arjunascagnetto <arjuna.scagnetto@gmail.com>
2015-05-12 10:04:43 +0200

    Seconda

--- PressFlow.py
+++ PressFlow.py
@@ -2,29 +2,56 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from collections import deque
+from matplotlib.font_manager import FontProperties
 
-
-# !!!! EVEN NUMBER ONLY !!!
-nsPlot = 10 # number of SubPlot 
+# zero for testing only
 plotVar = 1
+
+def nta_file_cleaner():
+    filename = '001_ServoCurveData_0000.nta'
+    f = open(filename,'r')
+    lines = f.readlines()
+    dat = deque()
+    for line in lines:
+        if not line.startswith('%'):
+            dat.append(line.replace('\n','').split('\t',8))
+    v = np.ndarray(shape=(len(dat),8),dtype=float)
+    for i in range(len(dat)):
+        for j in range(3,7): # prende solo dalla colonna 4 alla 7
+            v[i,j]=float(dat[i][j])
+    return v
+
+def ndata(data2norm):
+    data2norm = data2norm/np.max(np.abs(data2norm))
+    return data2norm
 
 def gen_subplot(Num_of_images,data):
     for i in range(0,Num_of_images):
+        fontP = FontProperties()
+        fontP.set_size('small')
         f, ax = plt.subplots(2)
         ax[0].set_xlabel('Pressione')
         ax[0].set_ylabel('Flusso')
         cycle = np.asarray(data[i])
-        time_len = len(cycle[:,0])
-        t = np.linspace(0,1,time_len)
-        print len(t),len(cycle[:,1])
-        ax[0].plot(cycle[:,1],cycle[:,2],'r')
-        ax[1].plot(t,cycle[:,1],t,cycle[:,2],t,cycle[:,3])
+        start = cycle[0,0]
+        end = cycle[-1,0]
+        t = np.arange(start,end+1,1)
+        ax[0].plot(cycle[:,1],cycle[:,2],'k')
+        ax[1].plot(t,ndata(cycle[:,1]),'b',label='Pressione')
+        ax[1].plot(t,ndata(cycle[:,2]),'r',label='Flusso')
+        ax[1].plot(t,ndata(cycle[:,3]),'g',label='Edi')
+        ax[1].legend(loc='lower right',prop = fontP)
+        ax[0].set_title(('from',str(start),'to',str(end)))
+        ax[1].set_xlabel('Tempo')
+        ax[1].set_ylim(-1.2,1.2)
+        ax[1].set_xlim(start,end)
         dir = os.path.dirname('images/')
         if not os.path.exists(dir):
             os.makedirs(dir)
-        filename = "".join(('images/image-','-',str(i)))
+        filename = "".join(('images/image','-',str(i+1)))
         print "Saving",filename 
-        plt.savefig(filename)
+        if plotVar:
+            plt.savefig(filename)
         plt.close()
 
 def build_data():
@@ -34,7 +61,7 @@ def build_data():
     length = len(rows)
     v = np.ndarray(shape=(length,4),dtype=float)
     for i in range(length):
-        v[i,:]=rows[i].replace('\n','').split('\t',3)
+        v[i,:] = rows[i].replace('\n','').split('\t',4)
     st = v[:,0] # States 0 inspirio 1 espirio
     pr = v[:,1] # Pressure
     fl = v[:,2] # Flow
@@ -59,10 +86,7 @@ def build_data():
     return d
 
 
+#v = nta_file_cleaner()
 deque = build_data()
 nIm = len(deque) # Number of Images
-print '\n',nIm,'immagini'
-print 'ULTIMO APPEND: ',deque[-1][-1]
-
-if plotVar:
-    gen_subplot(nIm,deque)
+gen_subplot(nIm,deque)
